407fcf6de1edc0761509d434e52f877771448b35
Grant McConnaughey <grant@britecore.com>
2016-07-16 17:02:17 -0500

    Add mccabe file

---
+++ mccabe.py
@@ -0,0 +1,61 @@
+def test_mccabe():
+    a = 1
+    b = 2
+    c = 3
+
+    if a < 1:
+        print 'Less than 1'
+    elif a < 2:
+        print 'Less than 2'
+    if a < 3:
+        print 'Less than 3'
+    elif a < 4:
+        print 'Less than 4'
+    if a < 5:
+        print 'Less than 5'
+    elif a < 6:
+        print 'Less than 6'
+    if a < 7:
+        print 'Less than 7'
+    elif a < 8:
+        print 'Less than 8'
+    elif a < 9:
+        print 'Less than 9'
+
+    if b < 1:
+        print 'Less than 1'
+    elif b < 2:
+        print 'Less than 2'
+    if b < 3:
+        print 'Less than 3'
+    elif b < 4:
+        print 'Less than 4'
+    if b < 5:
+        print 'Less than 5'
+    elif b < 6:
+        print 'Less than 6'
+    if b < 7:
+        print 'Less than 7'
+    elif b < 8:
+        print 'Less than 8'
+    elif b < 9:
+        print 'Less than 9'
+
+    if c < 1:
+        print 'Less than 1'
+    elif c < 2:
+        print 'Less than 2'
+    if c < 3:
+        print 'Less than 3'
+    elif c < 4:
+        print 'Less than 4'
+    if c < 5:
+        print 'Less than 5'
+    elif c < 6:
+        print 'Less than 6'
+    if c < 7:
+        print 'Less than 7'
+    elif c < 8:
+        print 'Less than 8'
+    elif c < 9:
+        print 'Less than 9'
