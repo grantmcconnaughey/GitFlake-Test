ab2a911cc4f54854ee662fdd1ebe66df427b2e8b
Grant McConnaughey <grant@britecore.com>
2016-07-30 16:54:37 -0500

    TEst

--- file_with_really_long_lines.py
+++ file_with_really_long_lines.py
@@ -4,3 +4,6 @@ def test_function():
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida leo sit amet elit facilisis vehicula. Etiam non quam leo. Nam imperdiet sapien quis felis consequat, ut faucibus augue ornare. Pellentesque volutpat et orci vitae dapibus. Aenean risus lacus, ornare id ex vulputate, feugiat venenatis massa. Vivamus finibus, neque sed aliquet ornare, enim enim facilisis nisi, in facilisis arcu justo at enim. Aliquam laoreet varius consectetur. Nam lobortis magna vitae viverra ornare. Nunc auctor consequat ligula sit amet scelerisque. Vestibulum mollis, orci id rutrum varius, sapien sem imperdiet lectus, aliquet euismod nisl magna id dui.
     """
     pass
+
+def tesT():
+    pass
